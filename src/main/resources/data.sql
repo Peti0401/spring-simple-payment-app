9c38d7d004b294b1c34166c8ea14a9304caafec8
Péter Gyarmati <gyarmatipeti01@gmail.com>
2020-02-25 09:53:57 +0100

    added mock account data to H2 database from data.sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into account (name, email, balance) values ('Clotilda Sarfas', 'csarfas0@mapquest.com', 200);
+insert into account (name, email, balance) values ('Barry Ardern', 'bardern1@nature.com', 200);
+insert into account (name, email, balance) values ('Vanessa Valentinetti', 'vvalentinetti2@google.ru', 200);
+insert into account (name, email, balance) values ('Quinlan Artois', 'qartois3@theglobeandmail.com', 200);
+insert into account (name, email, balance) values ('Nata Eisak', 'neisak4@howstuffworks.com', 200);
+insert into account (name, email, balance) values ('Lacy Krauze', 'lkrauze5@uol.com.br', 200);
+insert into account (name, email, balance) values ('Tabb Dunham', 'tdunham6@shutterfly.com', 200);
+insert into account (name, email, balance) values ('Rosalinda Moorfield', 'rmoorfield7@ed.gov', 200);
+insert into account (name, email, balance) values ('Margette Britzius', 'mbritzius8@usa.gov', 200);
+insert into account (name, email, balance) values ('Boonie Wigfield', 'bwigfield9@drupal.org', 200);
+
