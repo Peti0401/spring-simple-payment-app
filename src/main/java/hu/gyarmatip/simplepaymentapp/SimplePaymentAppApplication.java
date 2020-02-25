d694350f550c9a099c37413c38d2df8db47eb47b
Péter Gyarmati <gyarmatipeti01@gmail.com>
2020-02-25 09:24:40 +0100

    initial commit

---
+++ src/main/java/hu/gyarmatip/simplepaymentapp/SimplePaymentAppApplication.java
@@ -0,0 +1,13 @@
+package hu.gyarmatip.simplepaymentapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplePaymentAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplePaymentAppApplication.class, args);
+    }
+
+}
