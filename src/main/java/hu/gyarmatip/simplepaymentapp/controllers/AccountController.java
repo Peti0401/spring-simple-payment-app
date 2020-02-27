b199f227c6a24183a8752a3d1fdfab5c65fd64eb
Péter Gyarmati <gyarmatipeti01@gmail.com>
2020-02-27 22:01:45 +0100

    AccountController created to expose AccountService RESTfully

---
+++ src/main/java/hu/gyarmatip/simplepaymentapp/controllers/AccountController.java
@@ -0,0 +1,41 @@
+package hu.gyarmatip.simplepaymentapp.controllers;
+
+import hu.gyarmatip.simplepaymentapp.dto.AccountDto;
+import hu.gyarmatip.simplepaymentapp.entity.Account;
+import hu.gyarmatip.simplepaymentapp.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/account-service")
+public class AccountController {
+
+    private AccountService accountService;
+
+    @Autowired
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<Account> save(@RequestBody AccountDto accountDto) {
+        Account savedAccount = accountService.save(accountDto);
+        return new ResponseEntity<>(savedAccount, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable Long id) {
+        accountService.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/accounts")
+    public ResponseEntity<List<Account>> findAllAccounts() {
+        return ResponseEntity.ok(accountService.findAllAccounts());
+    }
+
+}
